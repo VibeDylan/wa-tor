c856026e50ea0fe2d391565206517f553b9aa1d6
VibeDylan <dylanbourbotte62@icloud.com>
2025-12-03 15:00:43 +0100

    define some public and private attribut

--- src/wator/planet.py
+++ src/wator/planet.py
@@ -1,3 +1,9 @@
 class Planet():
     def __init__(self, width: int, height: int):
-        pass
+        self.width = width
+        self.height = height
+        
+        self._grid: list[list[object | None]] = [
+            [None for _ in range(width)]
+            for _ in range(height)
+        ]
