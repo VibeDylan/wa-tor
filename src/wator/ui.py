3ad0197a82ee9985176e39871abf27c50fe8e651
VibeDylan <dylanbourbotte62@icloud.com>
2025-12-03 19:41:11 +0100

    add default button for start, pause and reset simulation

--- src/wator/ui.py
+++ src/wator/ui.py
@@ -4,6 +4,9 @@ class PlanetUI:
     def __init__(self, planet, cell_size=25):
         self.planet = planet
         self.cell_size = cell_size
+        
+        self.chronon = 0;
+        self.running = False
 
         self.root = tk.Tk()
         self.root.title("Wa-Tor Simulation")
@@ -20,13 +23,22 @@ class PlanetUI:
         self.sidebar.grid(row=0, column=1, padx=10, pady=10, sticky="n")
 
         self.label_chronon = tk.Label(self.sidebar, text="Chronon : 0", font=("Arial", 14))
-        self.label_chronon.pack(anchor="w")
+        self.label_chronon.pack(anchor="w", pady=5)
 
         self.label_fish = tk.Label(self.sidebar, text="Fish alive : 0", font=("Arial", 14))
-        self.label_fish.pack(anchor="w")
+        self.label_fish.pack(anchor="w", pady=5)
 
         self.label_shark = tk.Label(self.sidebar, text="Shark alive : 0", font=("Arial", 14))
-        self.label_shark.pack(anchor="w")
+        self.label_shark.pack(anchor="w", pady=5)
+        
+        self.button_start = tk.Button(self.sidebar, text="Start", command=self.start)
+        self.button_start.pack(fill="x", pady=5)
+
+        self.button_pause = tk.Button(self.sidebar, text="Pause", command=self.pause)
+        self.button_pause.pack(fill="x", pady=5)
+
+        self.button_reset = tk.Button(self.sidebar, text="Reset", command=self.reset)
+        self.button_reset.pack(fill="x", pady=5)
 
     def draw_grid(self):
         self.canvas.delete("all")
@@ -55,3 +67,18 @@ class PlanetUI:
     def refresh(self):
         self.draw_grid()
         self.root.update()
+        
+    def start(self):
+        """Lance la simulation (si pas déjà en cours)."""
+        if not self.running:
+            self.running = True
+            self._run_loop()
+
+    def pause(self):
+        """Met la simulation en pause."""
+        self.running = False
+
+    def reset(self):
+        """Réinitialise la simulation."""
+        # Arrêter si ça tourne
+        self.running = False
