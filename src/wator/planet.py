5d09c063da8e04ddfb1eb9548b789efcafd6e9bd
VibeDylan <dylanbourbotte62@icloud.com>
2025-12-03 16:11:17 +0100

    create some first features about planet and grid

--- src/wator/planet.py
+++ src/wator/planet.py
@@ -1,4 +1,4 @@
-class Planet():
+class Planet:
     def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
@@ -7,3 +7,24 @@ class Planet():
             [None for _ in range(width)]
             for _ in range(height)
         ]
+        
+    # ----- ACCÈS BASIQUE -----
+    def get(self, x: int, y: int):
+        """Retourne le contenu de la case."""
+        return self._grid[x][y]
+    
+    def set(self, x: int, y: int, entity) -> None:
+        """Place une entité dans la case (ou None)."""
+        self._grid[y][x] = entity
+       
+    # ---- OUTILS INTERNE -----
+    def wrap(self, x: int, y: int) -> tuple[int, int]: 
+        """Applique les règles toroïdales."""
+        return x % self.width, y % self.height
+    
+    def is_free(self, x: int, y: int) -> bool:
+        """Vrai si la case est vide."""
+        return self.get(x, y) is None 
+    
+        
+    
