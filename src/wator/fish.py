c68aa2fa60befa8ccd679c7cbed73a5815396de0
Fatima <you@example.com>
2025-12-04 15:49:58 +0100

    fix: add functions

--- src/wator/fish.py
+++ src/wator/fish.py
@@ -3,37 +3,38 @@ from .planet import Planet
 
 
 class Fish:
-    def __init__(self, x, y, age=0, reproduction_time=3):
+    def __init__(self, x : int, y : int, age=0, reproduction_time=3):
         self.x = x
         self.y = y
         self.age = age
         self.reproduction_time = reproduction_time
 
-    def ask_direction(self, planet: Planet) -> tuple[int, int] | None: 
+
+    def move(self, planet: Planet, new_position: tuple[int, int]) -> None:
+        old_position = (self.x, self.y)
+        move_allowed = planet.move(self.x, self.y, new_position[0], new_position[1])
+        if move_allowed:
+            self.age += 1
+            self.x = new_position[0]
+            self.y = new_position[1]
+            self.reproduce(planet, old_position)
+
+
+    def ask_direction(self, planet: Planet)  -> None:
         free_cells = planet.free_neighbors(self.x, self.y)
         if free_cells:
-            return random.choice(free_cells)
-        return None
-
-    def move(self, planet: Planet):
-        new_position = self.ask_direction(planet)
-        if new_position:
-            new_x, new_y = new_position
-            moved = planet.move(self.x, self.y, new_x, new_y)
-            if moved:
-                self.x, self.y = new_x, new_y
-
-    def reproduce(self, planet: Planet): 
+            free_random = random.choice(free_cells)
+            print(free_random)
+            self.move(planet, free_random)
         self.age += 1
-        if self.age >= self.reproduction_time:
-            free_cells = planet.free_neighbors(self.x, self.y)
-            if free_cells:
-                new_x, new_y = random.choice(free_cells)
-                baby_fish = Fish(new_x, new_y, age=0, reproduction_time=self.reproduction_time)
-                planet.add(baby_fish, new_x, new_y)
-                self.age = 0
-                return baby_fish
-        return None
+
+
+    def reproduce(self, planet: Planet, old_position: tuple[int, int]) -> None:
+        if self.age > 0 and self.age % self.reproduction_time == 0:
+            baby_shark = Shark(x=old_position[0], y=old_position[1])
+            planet.add(baby_shark, baby_shark.x, baby_shark.y)
+
 
     def __str__(self):
         return f"Fish(x={self.x}, y={self.y}, age={self.age})"
+
--- src/wator/shark.py
+++ src/wator/shark.py
@@ -2,7 +2,8 @@ import random
 from .planet import Planet
 
 class Shark:
-    def __init__(self, x: int, y:int, reproduction_time: int=5, energy: int=10, age: int=0):
+    def __init__(self, id: int, x: int, y:int, reproduction_time: int=5, energy: int=10, age: int=0):
+        self.id = id
         self.x = x
         self.y = y
         self.age = age
@@ -14,8 +15,8 @@ class Shark:
         self.energy += energy
 
     def reproduce(self, planet: Planet, old_position: tuple[int, int]) -> None:
-        if self.age > 0 and self.age % self.reproduction_time == 0:
-            baby_shark = Shark(x=old_position[0], y=old_position[1])
+        if self.age % self.reproduction_time == 0:
+            baby_shark = Shark(id=2, x=old_position[0], y=old_position[1])
             planet.add(baby_shark, baby_shark.x, baby_shark.y)
 
     def change_position(self, planet: Planet, new_position:tuple[int, int], eating: bool) -> None:
@@ -23,11 +24,15 @@ class Shark:
         move_allowed = planet.move(self.x, self.y, new_position[0], new_position[1])
         energy = 5 if eating else -1
         if move_allowed:
+            if eating:
+                planet.remove(new_position[0], new_position[1])
+            planet.set(self, new_position[0], new_position[1])
             self.update_stats(energy)
             self.x = new_position[0]
             self.y = new_position[1]
             self.reproduce(planet, old_position)
-        # Si le mouvement n'est pas autorisé, le requin reste sur place et perd de l'énergie
+        else:
+            self.ask_direction()
 
     def move(self, planet: Planet, new_position: tuple[int, int]) -> None:
         eating = False
@@ -39,21 +44,21 @@ class Shark:
 
     def ask_direction(self, planet: Planet) -> None:
         def generate_random_index(liste: list) -> int:
-            i = random.randint(0, len(liste) - 1)
+            i = random.randint(0, len(liste))
             return i
 
         adjacent_fishes = planet.fish_neighbors(self.x, self.y)
         if len(adjacent_fishes) == 1:
-            self.eat(planet, adjacent_fishes[0])
+            self.eat(adjacent_fishes[0])
         elif len(adjacent_fishes) > 1:
             fish_random = generate_random_index(adjacent_fishes)
-            self.eat(planet, adjacent_fishes[fish_random])
+            self.eat(adjacent_free[fish_random])
         else:
             adjacent_free = planet.free_neighbors(self.x, self.y)
             if len(adjacent_free) == 1:
-                self.move(planet, adjacent_free[0])
+                self.move(adjacent_free[0])
             elif len(adjacent_free) > 1:
                 free_random = generate_random_index(adjacent_free)
-                self.move(planet, adjacent_free[free_random])
+                self.move(adjacent_free[free_random])
             else:
                 self.update_stats(-1)
