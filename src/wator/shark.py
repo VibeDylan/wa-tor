fb30e1f9f46bbcdfa2ae49234069485d1b119b81
Flora T <flora.trecul@gmail.com>
2025-12-04 16:09:10 +0100

    Fix: implement shark inheritance from fish

--- src/wator/shark.py
+++ src/wator/shark.py
@@ -1,60 +1,31 @@
 import random
 from .planet import Planet
+from .fish import Fish
 
-class Shark:
-    def __init__(self, x: int, y:int, reproduction_time: int=5, energy: int=10, age: int=0):
-        self.x = x
-        self.y = y
-        self.age = age
+class Shark(Fish):
+    def __init__(self, x, y, age, reproduction_time: int=5, energy: int=10):
+        super().__init__(x, y, age)
         self.reproduction_time = reproduction_time
         self.energy = energy
-    
-    def update_stats(self, energy: int):
-        self.age += 1
-        self.energy += energy
-
-    def reproduce(self, planet: Planet, old_position: tuple[int, int]) -> None:
-        if self.age % self.reproduction_time == 0:
-            baby_shark = Shark(x=old_position[0], y=old_position[1])
-            planet.add(baby_shark, baby_shark.x, baby_shark.y)
-
-    def change_position(self, planet: Planet, new_position:tuple[int, int], eating: bool) -> None:
-        old_position = (self.x, self.y)
-        move_allowed = planet.move(self.x, self.y, new_position[0], new_position[1])
-        energy = 5 if eating else -1
-        if move_allowed:
-            self.update_stats(energy)
-            self.x = new_position[0]
-            self.y = new_position[1]
-            self.reproduce(planet, old_position)
-        else:
-            self.ask_direction(planet)
-
-    def move(self, planet: Planet, new_position: tuple[int, int]) -> None:
-        eating = False
-        self.change_position(planet, new_position, eating)
 
     def eat(self, planet: Planet, new_position: tuple[int, int]) -> None:
-        eating = True
-        self.change_position(planet, new_position, eating)
+        self.move(planet, new_position)
+        if self.energy >= 7:
+            self.energy = 10
+        else:
+            self.energy += 3
 
-    def ask_direction(self, planet: Planet) -> None:
-        def generate_random_index(liste: list) -> int:
-            i = random.randint(0, len(liste) - 1)
-            return i
+    def check_if_dead(self, planet: Planet) -> None:
+        if self.energy == 0:
+            planet.remove(self.x, self.y)
 
+    def search_fish(self, planet: Planet) -> None:
         adjacent_fishes = planet.fish_neighbors(self.x, self.y)
         if len(adjacent_fishes) == 1:
             self.eat(planet, adjacent_fishes[0])
         elif len(adjacent_fishes) > 1:
-            fish_random = generate_random_index(adjacent_fishes)
-            self.eat(planet, adjacent_fishes[fish_random])
+            self.eat(planet, random.choice(adjacent_fishes))
         else:
-            adjacent_free = planet.free_neighbors(self.x, self.y)
-            if len(adjacent_free) == 1:
-                self.move(planet, adjacent_free[0])
-            elif len(adjacent_free) > 1:
-                free_random = generate_random_index(adjacent_free)
-                self.move(planet, adjacent_free[free_random])
-            else:
-                self.update_stats(-1)
+            self.ask_direction(planet)
+            self.energy -= 1
+            self.check_if_dead()
