58786e42754d72e45b13fa95d5233a687a37ad02
VibeDylan <dylanbourbotte62@icloud.com>
2025-12-04 20:37:17 +0100

    add wrap testing

--- tests/test_planet.py
+++ tests/test_planet.py
@@ -7,3 +7,23 @@ class TestPlanetInitialisation():
         
         assert len(grid) == 5, "La grille doit avoir 5 lignes"
         assert len(grid[0]) == 5, "Chaque ligne doit avoir 5 colonnes"
+        
+class TestPlanetWrap():
+    def test_wrap_negative(self):
+        planet = Planet(5, 5)
+        
+        result = planet.wrap(-1, 2)
+        assert(result == (4, 2))
+        
+    def test_wrap_overflow(self):
+        planet = Planet(5, 5)
+        result = planet.wrap(5, 3)
+        
+        assert(result == (0, 3))
+        
+    def test_wrap_inside_bounds(self):
+        planet = Planet(5,5)
+        
+        result = planet.wrap(3, 2)
+        
+        assert(result == (3,2))
