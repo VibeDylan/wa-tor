718d3655d73e96a8acf059f1b058fb230aa5bed2
VibeDylan <dylanbourbotte62@icloud.com>
2025-12-04 14:18:02 +0100

    fix critical bug

--- src/wator/shark.py
+++ src/wator/shark.py
@@ -1,5 +1,5 @@
 import random
-from planet import Planet
+from .planet import Planet
 
 class Shark:
     def __init__(self, x: int, y:int, reproduction_time: int=5, energy: int=10, age: int=0):
@@ -23,15 +23,12 @@ class Shark:
         move_allowed = planet.move(self.x, self.y, new_position[0], new_position[1])
         energy = 5 if eating else -1
         if move_allowed:
-            if eating:
-                planet.remove(new_position[0], new_position[1])
-            planet.set(self, new_position[0], new_position[1])
             self.update_stats(energy)
             self.x = new_position[0]
             self.y = new_position[1]
             self.reproduce(planet, old_position)
         else:
-            self.ask_direction()
+            self.ask_direction(planet)
 
     def move(self, planet: Planet, new_position: tuple[int, int]) -> None:
         eating = False
@@ -43,21 +40,21 @@ class Shark:
 
     def ask_direction(self, planet: Planet) -> None:
         def generate_random_index(liste: list) -> int:
-            i = random.randint(0, len(liste))
+            i = random.randint(0, len(liste) - 1)
             return i
 
         adjacent_fishes = planet.fish_neighbors(self.x, self.y)
         if len(adjacent_fishes) == 1:
-            self.eat(adjacent_fishes[0])
+            self.eat(planet, adjacent_fishes[0])
         elif len(adjacent_fishes) > 1:
             fish_random = generate_random_index(adjacent_fishes)
-            self.eat(adjacent_free[fish_random])
+            self.eat(planet, adjacent_fishes[fish_random])
         else:
             adjacent_free = planet.free_neighbors(self.x, self.y)
             if len(adjacent_free) == 1:
-                self.move(adjacent_free[0])
+                self.move(planet, adjacent_free[0])
             elif len(adjacent_free) > 1:
                 free_random = generate_random_index(adjacent_free)
-                self.move(adjacent_free[free_random])
+                self.move(planet, adjacent_free[free_random])
             else:
                 self.update_stats(-1)
