4817d0971456b9542588c2009d45bffef595edf1
VibeDylan <dylanbourbotte62@icloud.com>
2025-12-05 16:12:00 +0100

    add some fix / bug

--- src/wator/fish.py
+++ src/wator/fish.py
@@ -8,42 +8,37 @@ if TYPE_CHECKING:
 
 
 class Fish:
-    def __init__(self, x : int, y : int, age=0, reproduction_time=3):
+    def __init__(self, x: int, y: int, age=0, reproduction_time=3):
         self.x = x
         self.y = y
         self.age = age
         self.reproduction_time = reproduction_time
     
 
-    def _move(self, planet: 'Planet', new_position: tuple[int, int]) -> None:
+    def move(self, planet: 'Planet', new_position: tuple[int, int]) -> None:
         old_position = (self.x, self.y)
         move_allowed = planet.move(self.x, self.y, new_position[0], new_position[1])
+
         if move_allowed:
-            self.age += 1
-            self.x = new_position[0]
-            self.y = new_position[1]
             self.reproduce(planet, old_position)
-        else:
-            self.ask_direction(planet)
-
-
-    def ask_direction(self, planet: 'Planet')  -> None:
-        free_cells = planet.free_neighbors(self.x, self.y)
-        if free_cells:
-            free_random = random.choice(free_cells)
-            print(free_random)
-            self.move(planet, free_random)
-        self.age += 1
 
 
     def reproduce(self, planet: 'Planet', old_position: tuple[int, int]) -> None:
         if self.age > 0 and self.age % self.reproduction_time == 0:
-            self.age == self.reproduction_time
-            baby_fish = Fish(x=old_position[0], y=old_position[1])
-            planet.add(baby_fish, baby_fish.x, baby_fish.y)
+            baby = Fish(old_position[0], old_position[1])
+            planet.add(baby, baby.x, baby.y)
 
+            
+    def step(self, planet: 'Planet') -> None:
+        # Le poisson vieillit au début du tour (logique Wa-Tor)
+        self.age += 1
+
+        free_cells = planet.free_neighbors(self.x, self.y)
+    
+        if free_cells:
+            new_position = random.choice(free_cells)
+            self.move(planet, new_position)
 
 
     def __str__(self):
         return f"Fish(x={self.x}, y={self.y}, age={self.age})"
-
