bba347387327681146c2c9bcd9572a0872b60c76
VibeDylan <dylanbourbotte62@icloud.com>
2025-12-09 10:58:12 +0100

    quickfix: time.sleep()

--- src/wator/config.py
+++ src/wator/config.py
@@ -1,8 +1,8 @@
-grid_width = 200
-grid_height = 100
-number_fishes = 60
-number_sharks = 30
-fish_reproduction = 2
+grid_width = 10
+grid_height = 10
+number_fishes = 5
+number_sharks = 1
+fish_reproduction = 5
 shark_reproduction = 15
 shark_energy = 10
 shark_energy_gain = 3
