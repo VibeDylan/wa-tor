4e7c6841411c14f526616b4c1da17801511c5388
Flora T <flora.trecul@gmail.com>
2025-12-10 11:26:50 +0100

    fix: use context manager to interact with database

--- src/wator/database.py
+++ src/wator/database.py
@@ -3,14 +3,14 @@ import sqlite3, datetime
 
 def connect_database():
     connection = sqlite3.connect("database_wator.db")
-    cursor = connection.cursor()
-    return connection, cursor
+    return connection
 
 
 def create_database():
-    connection, cursor = connect_database()
+    connection = connect_database()
     
-    cursor.execute("""
+    with connection:
+        connection.execute("""
     CREATE TABLE IF NOT EXISTS simulation(
             id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
             datetime TEXT NOT NULL,
@@ -20,27 +20,27 @@ def create_database():
         )
     """)
 
-    connection.commit()
     connection.close()
 
 
 def archive_simulation(nb_chronon: int, nb_fish: int, nb_sharks: int):
-    connection, cursor = connect_database()
+    connection = connect_database()
     
     date = datetime.datetime.now()
     formatted_date = date.strftime("%Y-%m-%d %H:%M")
     simulation_data = {"datetime": formatted_date, "chronons": nb_chronon, "fishes": nb_fish, "sharks": nb_sharks}
     
-    cursor.execute("""
-        INSERT INTO simulation(datetime, chronons, fishes, sharks) VALUES (:datetime, :chronons, :fishes, :sharks)
-    """, simulation_data)
+    with connection:
+        connection.execute("""
+            INSERT INTO simulation(datetime, chronons, fishes, sharks) VALUES (:datetime, :chronons, :fishes, :sharks)
+        """, simulation_data)
 
-    connection.commit()
     connection.close()
 
 
 def display_history():
-    connexion, cursor = connect_database()
+    connection = connect_database()
+    cursor = connection.cursor()
     
     cursor.execute("""SELECT * FROM simulation""")
 
@@ -48,4 +48,4 @@ def display_history():
     for row in last_results:
         print("Simulation n°{0} ({1}) lasted {2} chronons with {3} fishes left and {4} sharks left.".format(row[0], row[1], row[2], row[3], row[4]))
     
-    connexion.close()
+    connection.close()
