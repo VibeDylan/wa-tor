2e0dad4e128ca8c8e7dd0b6e91cc92e2eea00864
Fatima <you@example.com>
2025-12-11 12:14:42 +0100

    fix: value error

--- src/wator/config.py
+++ src/wator/config.py
@@ -1,7 +1,7 @@
 grid_width = 30
-grid_height = 25
-number_fishes = 10
-number_sharks = 10
+grid_height = 30
+number_fishes = 300
+number_sharks = 100
 fish_reproduction = 5
 shark_reproduction = 15
 shark_energy = 10
